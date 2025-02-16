06f7a07ffa6a23f8c2e619d35bd34c2b4b9b109e
Bilegt Davaa <bilegt.davaa@lcieducation.net>
2025-02-16 03:52:47 -0500

    S3 versioning

--- 1/main.tf
+++ 1/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "Slimes-bucket"
+  bucket = "slimes-bucket"
 
   #Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -29,4 +29,25 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
       }
     }
   
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+
+  
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
 }
