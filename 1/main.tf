d5891e7444fa297bc941a05d610d9dd57e8b3f17
Bilegt Davaa <bilegt.davaa@lcieducation.net>
2025-02-16 03:35:21 -0500

    s3 bucket

---
+++ 1/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "us-east-2"
+  
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "Slimes-bucket"
+
+  #Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  
+}
